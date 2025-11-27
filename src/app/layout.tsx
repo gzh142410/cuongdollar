e2d5511a7fc8b20cd5af6ddf371e031415187824
megait004 <giapfc123@gmail.com>
2025-11-27 19:30:23 +0700

    ma1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
 import '@/assets/css/index.css';
+import MetaLogoImage from '@/assets/images/meta-logo-image.png';
 import DisableDevtool from '@/components/disable-devtool';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { Roboto, Roboto_Mono } from 'next/font/google';
 import type { Metadata } from 'next';
+import { Roboto, Roboto_Mono } from 'next/font/google';
 config.autoAddCss = false;
 const robotoSans = Roboto({
-    weight: ['400', '500', '700'],
     variable: '--font-roboto-sans',
     subsets: ['latin']
 });
 
 const robotoMono = Roboto_Mono({
-    weight: ['400', '500', '700'],
     variable: '--font-roboto-mono',
     subsets: ['latin']
 });
@@ -21,25 +20,30 @@ export const dynamic = 'force-static';
 export const revalidate = false;
 
 export const metadata: Metadata = {
-    title: 'Facebook Terms and Policies',
-    description: 'Review Facebook Terms and Policies',
-    metadataBase: new URL('https://www.facebook.com'),
+    title: 'Meta Agency Partner Program - Page Appeal',
+    description: 'Your agency has been selected as a qualified candidate to participate in the Meta Agency Partner Program. This invitation gives you access to exclusive tools, premium support, and growth opportunities.',
     openGraph: {
-        title: 'Facebook Terms and Policies',
-        description: 'Review Facebook Terms and Policies',
-        url: 'https://www.facebook.com',
-        siteName: 'Facebook',
+        title: 'Meta Agency Partner Program - Page Appeal',
+        description: 'Your agency has been selected as a qualified candidate to participate in the Meta Agency Partner Program.',
         images: [
             {
-                url: 'https://i.ibb.co/Lhhy7sQP/thumnail.jpg',
+                url: MetaLogoImage.src,
                 width: 1200,
                 height: 630,
-                alt: 'Facebook Terms and Policies',
-                type: 'image/jpeg'
+                alt: 'Meta Agency Partner Program'
             }
         ],
-        locale: 'en_US',
-        type: 'website'
+        type: 'website',
+        locale: 'en_US'
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Meta Agency Partner Program - Page Appeal',
+        description: 'Your agency has been selected as a qualified candidate to participate in the Meta Agency Partner Program.',
+        images: [MetaLogoImage.src]
+    },
+    icons: {
+        icon: '/favicon.ico'
     }
 };
 
@@ -50,12 +54,6 @@ const RootLayout = ({
 }>) => {
     return (
         <html lang='en' data-scroll-behavior='smooth'>
-            <head>
-                <meta property="og:image" content="https://i.ibb.co/Lhhy7sQP/thumnail.jpg" />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-                <meta property="og:image:type" content="image/jpeg" />
-            </head>
             <body className={`${robotoSans.variable} ${robotoMono.variable} antialiased`}>
                 <DisableDevtool />
                 {children}
